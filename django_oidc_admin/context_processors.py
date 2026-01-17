61a322f77c7ca06b5328436d33ecbc700902c1e9
Roman Geber <dlz9zxdkjptu@romangeber.com>
2026-01-17 05:05:10 +0100

    Added capability to use custom user models
    Removed usage of the `User` Model in favour of `get_user_model` to
    honour changes made to the Django setting `AUTH_USER_MODEL`
    Added new config paramter `OIDCADMIN_USER_LIST_URL_NAME` defaulting to
    `admin:auth_user_changelist`. Orerride in order to point ot a custom
    user list. This fixes errors on login when a custom user model is used
    Added template tag to create user admin URL
    Removed `views.py` as it wasn't in use.

--- django_oidc_admin/context_processors.py
+++ django_oidc_admin/context_processors.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 def admin_navbar(request):
     """Add the number of pending SSO users to the available context in templates"""
     if request.user.is_staff:
         return {
-            "pending_sso_users": User.objects.filter(is_active=False).count(),
+            "pending_sso_users": get_user_model().objects.filter(is_active=False).count(),
         }
     return {}
